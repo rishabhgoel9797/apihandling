51612638c26c63be67ead841a03deacedda75151
Rishabh Goel <rishu@Rishus-MacBook-Pro.local>
2019-03-08 18:24:07 +0530

    Frontend Setup

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const state = {
+
+}
+const getters = {
+
+}
+const actions = {
+
+}
+const mutations = {
+
+}
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations
+})
